3dc5008a8a49c8b6041a3b9141f764bba949e529
wangjungang1994 <1499370499@qq.com>
2016-12-02 00:29:06 +0800

    第一次提交

---
+++ swift2-1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  swift2-1
+//
+//  Created by 王俊钢 on 16/9/22.
+//  Copyright © 2016年 wangjungang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+}
